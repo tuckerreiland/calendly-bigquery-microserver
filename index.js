3a3a6753441be15e02c3ea324bcfc7acf8cff254
tuckerreiland <treiland@uw.edu>
2023-04-05 15:13:06 -0600

    working on updates to microserver architecture

--- index.js
+++ index.js
@@ -19,48 +19,66 @@ app.post('/webhook', async (req, res) => {
 	const data = req.body
 	fetchEventData(data)
 	.then(calendlyEvent => {
+		// console.log('============CALENDLY EVENT===============')
+		// console.log(calendlyEvent)
+		// res.send(calendlyEvent)
+		// return calendlyEvent
 		formattedCalendlyData = new FormattedData (calendlyEvent)
 		res.send(formattedCalendlyData)
 		return formattedCalendlyData
 	})
-	.then(dataForBiqQuery => {
-		main(dataForBiqQuery)
-		console.log(dataForBiqQuery)
-	})
+	// .then(dataForBiqQuery => {
+	// 	main(dataForBiqQuery)
+	// 	console.log(dataForBiqQuery)
+	// })
 })
 
 app.listen(PORT, () => {
-	console.log(`linstening on ${PORT}`)
+	console.log(`listening on ${PORT}`)
 })
 
 // Calendly Fetch
 // =============================================================
 const BearerToken = process.env.CALENDLY_BEARER_TOKEN
+const organizationUri = 'https://api.calendly.com/organizations/CEBBNBYN7S5RBBCM'
+
 
 let calendlyFetchOptions = {
 	method: 'GET',
 	headers: {'Content-Type': 'application/json', 
 	Authorization: `${BearerToken}`}
 };
+const url = `https://api.calendly.com/event_types/BDFPR46JKT67V5RT?organization=${organizationUri}`
+
+fetch(url, calendlyFetchOptions)
+	.then(res => res.json())
+	.then(json => console.log(json))
+	.catch(err => console.error('error:' + err));
 
 const fetchEventData = (data) => {
-	console.log(data.payload.event)
-	return fetch(data.payload.event, calendlyFetchOptions)
-	.then(res => {return res.json()})
-	.then( event => {
-		return fetch(event.resource.event_type, calendlyFetchOptions)
+	return fetch(data.payload.uri, calendlyFetchOptions)
 		.then(res => {return res.json()})
-		.then(eventType => {
-			const webhookData = data.payload
-			const fullName = webhookData.name.split(' ')
-			webhookData.first_name = fullName[0]
-			webhookData.last_name = fullName[1]
-			webhookData.event = event.resource
-			webhookData.event.event_type = eventType.resource
-			console.log(webhookData)
-			return webhookData
+		.then( uri => {
+			// console.log('============URI===============')
+			// 	console.log(uri)
+			return fetch(uri.resource.event, calendlyFetchOptions)
+			.then(res => {return res.json()})
+			.then( event => {
+				console.log('============URI===============')
+				console.log(uri)
+				console.log('============EVENT===============')
+				console.log(event)
+				const webhookData = data.payload
+				const fullName = webhookData.name.split(' ')
+				webhookData.first_name = fullName[0]
+				webhookData.last_name = fullName[1]
+				webhookData.event = event.resource
+				webhookData.questions_and_answers = uri.resource.questions_and_answers
+				console.log('============FINAL===============')
+				console.log(webhookData)
+				return webhookData	
+			})
 		})
-	})
 	.catch(err => console.log('error:' + err));
 };
 
@@ -71,24 +89,27 @@ class FormattedData {
 	constructor (data) {
 		this.created_at = data.event.created_at;
 		this.appointment_time = data.event.start_time;
-		this.duration = data.event.event_type.duration;
-		this.appointment_type = data.event.event_type.name;
+		this.duration = data.event.duration;
+		this.appointment_type = data.event.name;
 		this.location = data.event.location.location;
 		this.first_name = data.first_name;
 		this.last_name = data.last_name;
 		this.email = data.email;
 		this.phone = data.text_reminder_number;
-		this.details = data.event.event_type.kind_description;
-		this.referral_source = data.tracking.utm_source;
-		this.referral_campaign = data.tracking.utm_campaign;
+		this.questions_and_answers = data.questions_and_answers
+		this.utm_campaign = data.tracking.utm_campaign;
+		this.utm_source = data.tracking.utm_source;
+		this.utm_medium = data.tracking.utm_medium;
+		this.utm_term = data.tracking.utm_term;
+		this.utm_content = data.tracking.utm_content;
 		this.notifications_phone = data.text_reminder_number;
-		this.employee = data.event.event_type.profile.name;
+		// this.employee = data.event.profile.name;
+			// TODO: Need to figure out API call for employee name and appointment location
 	}
 }
 
-// Formatted Data Constructor
+// Big Query Connection
 // =============================================================
-'use strict';
 
 function main(dataForBiqQuery) {
     // Import the Google Cloud client libraries
